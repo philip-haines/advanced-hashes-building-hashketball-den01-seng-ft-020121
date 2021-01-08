01e8c8285a87df47114883ebe4ed7934ec7acc22
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-08 21:57:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,12 @@ def game_hash
   game_hash = {
     :home => {
       :team_name => {},
-      :colors => {},
+      :colors => {}
     },
-    :away => {}
+    :away => {
+      :team_name => {},
+      :colors => {}
+    }
   }
   game_hash
 end
