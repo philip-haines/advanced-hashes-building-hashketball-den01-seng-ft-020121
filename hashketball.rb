3d9e741f10461e6ecab6d41e7427fb0768d0523c
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-08 22:19:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,21 +9,21 @@ def game_hash
       :players => [
           {
             :player_name => "Alan Anderson",
-            :number => 0
-            :shoe => 16
-            :points => 22
-            :rebounds => 12
-            :assists => 12
-            :steals => 3
-            :blocks => 1
+            :number => 0,
+            :shoe => 16,
+            :points => 22,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 3,
+            :blocks => 1,
             :slam_dunks => 1
           }, 
           
           {
             :player_name => "Reggie Evans",
-            :number => 30
-            :shoe => 14
-            :points => 12
+            :number => 30,
+            :shoe => 14,
+            :points => 12,
             :rebounds => 12
             :assists => 12
             :steals => 12
